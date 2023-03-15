3db0df22ba173649cf19ebda91e3b3a4b156c8df
Yamen kayyal <alkayyalyamen@gmail.com>
2023-03-15 15:10:04 +0300

    update 15\3

--- Outlet/Cellular/ICellular.py
+++ Outlet/Cellular/ICellular.py
@@ -3,8 +3,8 @@ from Outlet.IOutlet import Outlet
 
 # noinspection PyAbstractClass
 class Cellular(Outlet):
-    _id, position, radius, power = 0,0,0,0
-    def __init__(self, agent, supported_services, services_list, vehicles_list,*args):
+
+    def __init__(self, agent, supported_services, services_list, vehicles_list, *args):
         super().__init__(*args)
         self.agent = agent
         self.supported_services = supported_services
