ec5be6460b01708245b6fcd1f533219b10defe68
taher almoussali <taher@mindimpulse.co>
2023-03-28 12:02:42 +0200

    gg

--- Environment/env_variables.py
+++ Environment/env_variables.py
@@ -1,41 +1,25 @@
 network_path = 'H:\\work_projects\\network_slicing\\ns\\Environment\\Area\\map.sumocfg'
-
 random_routes_path = 'H:\\work_projects\\network_slicing\\ns\\Environment\\Area\\map.rou.xml'
 
-types_outlets = ['3G', '4G', '5G', 'SAT', 'wifi', 'UAV']
+types_outlets = ['3G', '4G', '5G', 'Wifi']
+
+outlet_radius = [1000] * 39
 
 outlets = {
-
     '3G': [],
-
     '4G': [],
-
     '5G': [],
-
-    'SAT': [],
-
     'wifi': [],
-
-    'UAV': [],
-
 }
-
 all_routes = []
 
 default_types_vehicles = [
-
     'DEFAULT_BIKETYPE',
-
     'DEFAULT_CONTAINERTYPE',
-
     'DEFAULT_PEDTYPE',
-
     'DEFAULT_TAXITYPE',
-
     'DEFAULT_VEHTYPE']
 
 # position_of_oultets = []
-
 vehicles_id_pos = []
-
 TIME = 100
