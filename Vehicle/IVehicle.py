c545065af5b6cd5bdb98f0e811f003c6966f88e7
Yamen kayyal <alkayyalyamen@gmail.com>
2023-03-28 13:41:32 +0300

    unit testing

--- Vehicle/IVehicle.py
+++ Vehicle/IVehicle.py
@@ -4,27 +4,24 @@ from typing import List
 
 class Vehicle(ABC):
     # TODO: replace int with road object in annotation
-    def __init__(self, _id: int,x,y, **kwargs):
+    def __init__(self, _id: int, x, y, **kwargs):
         """
-           Parameters
-           ----------
-            speed : float
-               The frequency used by the service.
-            position : list
-               x,y coordinates of the vehicle in the environment
-            acceleration : float
-               at how mush rate the vehicle increments it's speed
-            path : list[Road]
-                roads path of the vehicle
-            services : list[Service]
+        Parameters
+        ----------
+         speed : float
+            The frequency used by the service.
+         position : list
+            x,y coordinates of the vehicle in the environment
+         acceleration : float
+            at how mush rate the vehicle increments it's speed
+         path : list[Road]
+             roads path of the vehicle
+         services : list[Service]
 
         """
-        #self.position = position
-        self.services = kwargs.get('services', [])
-        self.outlets_serve=[]
+        # self.position = position
+        self.services = kwargs.get("services", [])
+        self.outlets_serve = []
         self.observers = []
         self.x = x
         self.y = y
-
-
-
