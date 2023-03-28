dd1791b4abebd1beeea56ae5ee86b18030f9674c
taher almoussali <taher@mindimpulse.co>
2023-03-28 12:01:05 +0200

    dd

---
+++ Environment/env_variables.py
@@ -0,0 +1,41 @@
+network_path = 'H:\\work_projects\\network_slicing\\ns\\Environment\\Area\\map.sumocfg'
+
+random_routes_path = 'H:\\work_projects\\network_slicing\\ns\\Environment\\Area\\map.rou.xml'
+
+types_outlets = ['3G', '4G', '5G', 'SAT', 'wifi', 'UAV']
+
+outlets = {
+
+    '3G': [],
+
+    '4G': [],
+
+    '5G': [],
+
+    'SAT': [],
+
+    'wifi': [],
+
+    'UAV': [],
+
+}
+
+all_routes = []
+
+default_types_vehicles = [
+
+    'DEFAULT_BIKETYPE',
+
+    'DEFAULT_CONTAINERTYPE',
+
+    'DEFAULT_PEDTYPE',
+
+    'DEFAULT_TAXITYPE',
+
+    'DEFAULT_VEHTYPE']
+
+# position_of_oultets = []
+
+vehicles_id_pos = []
+
+TIME = 100
