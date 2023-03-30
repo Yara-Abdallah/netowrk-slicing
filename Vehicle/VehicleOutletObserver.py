8437afae6525aaa972901b283fb4cce715750e15
yara <yara@mindimpulse.co>
2023-03-30 17:09:42 +0200

    logging car request

--- Vehicle/VehicleOutletObserver.py
+++ Vehicle/VehicleOutletObserver.py
@@ -1,4 +1,6 @@
 from Environment.env_variables import outlet_radius
+from Outlet.Cellular.FactoryCellular import FactoryCellular
+
 
 class Observer:
     def update(self, subject):
@@ -15,10 +17,16 @@ class ConcreteObserver(Observer):
         for i, o in enumerate(self.outlet_pos):
             if (subject.x >= (o[0] - self.outlet_raduis[i]) and subject.x <= (o[0] + self.outlet_raduis[i])) and (
                     subject.y >= (o[1] - self.outlet_raduis[i]) and subject.y <= (o[1] + self.outlet_raduis[i])):
-                subject.outlets_serve.append(o)
+
+
+                #print(self.outlet_pos)
+                factory = FactoryCellular(1, 1, [1, 1, 0], [o[0], o[1]], 10000, [10, 20, 30],
+                                          [10, 10, 10])
+
+                outlet = factory.produce_cellular_outlet("5G")
+                subject.outlets_serve.append(outlet)
             else:
                 pass
-        print("the serve outlets    .......    ", len(subject.outlets_serve))
 
     def update(self, subject):
         self.check(subject)
