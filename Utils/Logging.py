15c63f21f92eee1b920336f852a7bbd331020f10
Yamen kayyal <alkayyalyamen@gmail.com>
2023-03-30 11:14:19 +0300

    fixed cost

--- Utils/Logging.py
+++ Utils/Logging.py
@@ -1,18 +1,19 @@
 import logging
+import functools
 
 # Define a decorator function to add logging to a method
 def log_method(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         class_name = args[0].__class__.__name__
         method_name = func.__name__
         logger = logging.getLogger(class_name)
-        logger.debug(f"Calling {method_name}() with arguments {args} {kwargs}")
         result = func(*args, **kwargs)
-        method_name = result.fget.__name__
-        logger.debug(f"{method_name}() returned {result}")
+        logger.info(f"{class_name}'s {method_name} is: {result:.2g} ")
+        logger.info(f"{method_name} returned {result}")
         return result
-
     return wrapper
+
 
 logging.basicConfig(filename='cost.log', level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
