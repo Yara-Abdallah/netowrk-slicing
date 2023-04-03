2e0762560548e1e6197e27b079b7abc6bbbe8bd9
yara <yara@mindimpulse.co>
2023-04-03 10:44:42 +0300

    model builder

--- RL/RLAlgorithms/Model.py
+++ RL/RLAlgorithms/Model.py
@@ -7,9 +7,8 @@ from numpy import ndarray
 from RL.RLMeta import RLMeta, rlabc
 
 
-
 class Model():
-    def __init__(self, state_size, action_size , activation_function, loss_function, optimization_algorithm,
+    def __init__(self, state_size, action_size, activation_function, loss_function, optimization_algorithm,
                  learning_rate, output_activation, **kwargs):
         self.state_size = state_size
         self.action_size = action_size
@@ -24,10 +23,11 @@ class Model():
         model_.add(Dense(24, input_dim=self.state_size, activation=self.activation_function))
         model_.add(Dense(24, activation=self.activation_function))
         model_.add(Dense(self.action_size, activation=self.output_activation))
-        model_.add(Reshape((3,2)))
+        model_.add(Reshape((3, 2)))
         model_.compile(loss=self.loss_function,
                        optimizer=self.optimization_algorithm(learning_rate=self.learning_rate))
         return model_
+
     def load(self, filename):
         """ Load model from file. """
         self.load_weights(filename)
--- RL/RLBuilder.py
+++ RL/RLBuilder.py
@@ -1,6 +1,7 @@
 from Outlet.Cellular.ThreeG import ThreeG
 from RL.AgentBuilder import ActionBuilder, AgentBuilder_
 from RL.EnvBuilder import EnvironmentBuilder
+from RL.ModelBuilder import ModelBuilder_
 from RL.RLAlgorithms.Centralized_DQN import DQN
 from RL.RLAlgorithms.Model import Model
 from keras.optimizers import Adam
@@ -10,19 +11,21 @@ from RL.RLEnvironment.State.CentralizedState import CentralizedState
 
 
 class RLBuilder:
-    def __init__(self,model,rl=None):
+    def __init__(self, rl=None):
+
         if rl is None:
-            self.rl = DQN(model)
+            self.rl = DQN()
         else:
             self.rl = rl
-    def __str__(self):
-        return f" agent : {self.rl.agents} , env : {self.rl.environment}"
-
+    @property
+    def model_(self):
+        return ModelBuilder(self.rl)
+    @property
     def environment(self):
         return EnvBuilder(self.rl)
 
+    @property
     def agent(self):
-
         return AgentBuilder(self.rl)
 
     def build(self):
@@ -34,8 +37,15 @@ class EnvBuilder(RLBuilder):
         super().__init__(rl)
 
     def build_env(self):
-        self.rl.environment = EnvironmentBuilder().state().build_state(CentralizedState())\
-            .reward().build_reward(CentralizedReward()).build()
+        self.rl.environment = (
+            EnvironmentBuilder()
+            .state()
+            .build_state(CentralizedState())
+            .reward()
+            .build_reward(CentralizedReward())
+            .build()
+        )
+        print("self.rl.environment", self.rl)
         return self
 
 
@@ -44,35 +54,33 @@ class AgentBuilder(RLBuilder):
         super().__init__(rl)
 
     def build_agent(self):
+        print("self.rl.environment", self.rl)
+
         self.rl.agents = AgentBuilder_().action().build_action().build()
         return self
 
+class ModelBuilder(RLBuilder):
+    def __init__(self,rl):
+        super().__init__(rl)
 
-model_ = Model(3, 6, 'relu', 'mse', Adam, 0.5, 'sigmoid').build_model()
-build = RLBuilder(model_)
-builder = build \
-    .agent() \
-        .build_agent() \
-    .environment()\
-        .build_env() \
-    .build()
+    def build_model(self):
+        self.rl.model = ModelBuilder_().state_size().build_state_size(3) \
+                          .action_size().build_action_size(4) \
+                          .loss_func().build_loss_func("mse") \
+                          .optimizer().build_optimizer(Adam) \
+                          .activation_func().build_Activation("relu") \
+                          .activation_func_output_layer().build_activation_output("sigmoid") \
+                          .learning_rate().build_Learning_rate(0.5) \
+                          .builder()
+
+        return self
+
+
+
+build = RLBuilder()
+builder = build.agent.build_agent().environment.build_env().model_.build_model().build()
 
 print(builder.agents)
 print(builder.environment)
-
-
-
-#print(mb.agents.action)
-
-#print(mb.environment.state)
-#print(mb.environment.reward)
-#print(mb.build())
-
-#c=CentralizedState()
-# state = mb.environment().build_env().state
-# action_obj , action_value = mb.agent().build_agent().build().chain(model_,[10.0, 15.0],0.1)
-# print(action_obj.execute(c, action_value))
-
-
-# action, action_value = agent.chain(model,state,0.1)
-# print(action.execute(c, action_value))
+print(builder.model)
+print("fuck", builder)
