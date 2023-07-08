69e492fa23bbeef16ccae1fb8f9ccabce5439041
yara <yara@mindimpulse.co>
2023-07-08 14:13:35 +0300

    decentralize with just exploration

--- Environment/run_env_rl.py
+++ Environment/run_env_rl.py
@@ -35,7 +35,7 @@ from Vehicle.VehicleOutletObserver import ConcreteObserver
 import matplotlib.pyplot as plt
 import matplotlib
 
-results_dir = os.path.join(sys.path[0], 'results5_tanh')
+results_dir = os.path.join(sys.path[0], 'results1_explor_decentralize')
 path6 = os.path.join(results_dir, 'centralized_weights')
 path7 = os.path.join(results_dir, 'decentralized_weights')
 path_memory_centralize = os.path.join(results_dir,'centralize_memory')
@@ -49,12 +49,12 @@ p5 = os.path.join(results_dir, 'qvalue_centralized')
 # centralize_qvalue_path = os.path.join(prev_results_dir, 'qvalue_centralized_for_plotting')
 # decentralize_qvalue_path = os.path.join(prev_results_dir, 'qvalue_decentralized_for_plotting')
 prev_results_3tanh_dir = "//content//drive//MyDrive//network_slicing//results3_tanh//"
-prev_results_4tanh_dir = "//content//drive//MyDrive//network_slicing//results4_tanh//"
+# prev_results_4tanh_dir = "//content//drive//MyDrive//network_slicing//results1_explor_decentralize//"
 
 prev_centralize_weights_path = os.path.join(prev_results_3tanh_dir,"centralized_weights//")
-prev_decentralize_weights_path = os.path.join(prev_results_4tanh_dir,"decentralized_weights//")
+# prev_decentralize_weights_path = os.path.join(prev_results_4tanh_dir,"decentralized_weights//")
 # prev_centralize_memory_path = os.path.join(prev_results_4tanh_dir,"centralize_memory//")
-prev_decentralize_memory_path = os.path.join(prev_results_4tanh_dir,"decentralize_memory//")
+# prev_decentralize_memory_path = os.path.join(prev_results_4tanh_dir,"decentralize_memory//")
 centralize_qvalue_path = os.path.join(results_dir,"qvalue_centralized_for_plotting//")
 decentralize_qvalue_path = os.path.join(results_dir,"qvalue_decentralized_for_plotting//")
 
