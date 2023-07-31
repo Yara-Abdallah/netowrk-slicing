e0ba8187371cfd89bac65835c77c46efbb12428b
yara <yara@mindimpulse.co>
2023-07-31 22:28:12 +0300

    plaaaaaaaaaaa

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -8,8 +8,8 @@ paths = ["ratio_of_occupancy_decentralized//ratio_of_occupancy","reward_accumila
 
 def paths_reading_from(outlet_number):
     filesname = []
-    for phase_number in range(3):
-        filesname.append(f"C://Users//Windows dunya//Downloads//action_masking_phase{phase_number+1}_period3_occ_serving_req//utility_decentralized//utility{outlet_number}.pkl")
+    for phase_number in range(4):
+        filesname.append(f"C://Users//Windows dunya//Downloads//action_masking_phase{phase_number+1}_period3_occ_serving_req//qvalue_decentralized_for_plotting//qvalue{outlet_number}.pkl")
     return filesname
 
 results_dir = os.path.join(sys.path[0],
@@ -25,7 +25,7 @@ os.makedirs(serving_ratio, exist_ok=True)
 os.makedirs(occupancy, exist_ok=True)
 
 def read_data(filesname,queue):
-    for i in range(3):
+    for i in range(4):
         with open(filesname[i], 'rb') as file:
             try:
                 while True:
--- serving_ratio_plotting.py
+++ serving_ratio_plotting.py
@@ -1,11 +1,11 @@
 from plotting.plotting import *
 
 deque =[ ]
-deque = read_data(paths_reading_from(outlet_number=2),deque)
+deque = read_data(paths_reading_from(outlet_number=0),deque)
 # Example usage:
-window_size = 100
+window_size = 200
 result = rolling_average(deque, window_size)
 print(len(deque))
 x_values = [i for i in range(len(result))]  # Adjust x-axis values
-plotting(x_values,result,window_size,'4G',serving_ratio,"serving_ratio")
+plotting(x_values,result,window_size,'wifi',serving_ratio,"serving_ratio")
 
