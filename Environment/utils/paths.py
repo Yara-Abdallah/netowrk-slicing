caf32c4f92f5ffffec1721625a7f3da0c0a43484
yara <yara@mindimpulse.co>
2023-07-31 15:39:17 +0300

    action masking , occu , serving req , period 3 , phase3

--- Environment/utils/paths.py
+++ Environment/utils/paths.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 results_dir = os.path.join(sys.path[0],
-                           'action_masking_phase2_period3_occ_serving_req')
+                           'action_masking_phase3_period3_occ_serving_req')
 centralized_weights = os.path.join(results_dir, 'centralized_weights')
 decentralized_weights = os.path.join(results_dir, 'decentralized_weights')
 path_memory_centralize = os.path.join(results_dir, 'centralize_memory')
@@ -31,8 +31,8 @@ sum_power_allocation_path = os.path.join(results_dir, "sum_power_allocation//")
 # "/content/drive/MyDrive/network_slicing/results3_tanh"
 # os.path.join(sys.path[0],"results3_tanh//results3_tanh//")
 # results1_explor_decentralize = "//content//drive//MyDrive//network_slicing//results2_explor_decentralize//"
-prev_results_dec_weight = "//content//drive//MyDrive//network_slicing//action_masking_phase1_period3_occ_serving_req"
-prev_results_dec_memory = "//content//drive//MyDrive//network_slicing//action_masking_phase1_period3_occ_serving_req"
+prev_results_dec_weight = "//content//drive//MyDrive//network_slicing//action_masking_phase2_period3_occ_serving_req"
+prev_results_dec_memory = "//content//drive//MyDrive//network_slicing//action_masking_phase2_period3_occ_serving_req"
 prev_centralize_weights_path = os.path.join(prev_results_3tanh_dir, "centralized_weights//")
 prev_decentralize_weights_path = os.path.join(prev_results_dec_weight,"decentralized_weights//")
 # prev_centralize_memory_path = os.path.join(prev_results_4tanh_dir,"centralize_memory//")
