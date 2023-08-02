dd60c9b44664391ed694dfb623c356714d964b7c
yara <yara@mindimpulse.co>
2023-08-02 10:55:05 +0300

    asrgas

--- ocuupancy_ratio_plotting.py
+++ ocuupancy_ratio_plotting.py
@@ -2,10 +2,11 @@
 from plotting.plotting import *
 
 deque = []
-deque = read_data(paths_reading_from(outlet_number=0),deque)
+deque = read_data(paths_reading_from(outlet_number=1),deque)
 print(len(deque))
-
-window_size = 10
+for i in deque:
+    print(i)
+window_size = 1
 result = rolling_average(deque, window_size)
 x_values = [i for i in range(len(result))]  # Adjust x-axis values
-plotting(x_values,result,window_size,'wifi',occupancy,"occupancy")
+plotting(x_values,result,window_size,'3G',occupancy,"occupancy")
